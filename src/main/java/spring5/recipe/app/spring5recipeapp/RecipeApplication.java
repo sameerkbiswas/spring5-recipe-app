0534855c931d427db6a7dc6677c5e612bd2d8eaa
Sameer <Sameer@Sameer-PC>
2021-01-24 23:02:18 +0530

    Renamed the class from Spring5RecipeAppApplication to RecipeApplication

--- src/main/java/spring5/recipe/app/spring5recipeapp/RecipeApplication.java
+++ src/main/java/spring5/recipe/app/spring5recipeapp/RecipeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Spring5RecipeAppApplication {
+public class RecipeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Spring5RecipeAppApplication.class, args);
+		SpringApplication.run(RecipeApplication.class, args);
 	}
 
 }
